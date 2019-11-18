c3fba15bd8328f6dfe0f0b77366e1c4d2b01c04a
nathanccarnelos <nathancoltinho@gmail.com>
2019-11-18 16:53:48 -0300

    teste deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/DevWebEComerce/' : '/',
+  runtimeCompiler: process.env.NODE_ENV !== 'production',
   'transpileDependencies': [
     'vuetify'
   ]
